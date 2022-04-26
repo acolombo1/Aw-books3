cc1fa655c75149718344e892f840ba2e379a329e
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-04-26 13:40:52 -0300

    remove script.js put init code inside window load eventlistener

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-/* eslint-disable max-classes-per-file */
 import displayClock from './modules/clock.js';
 import Bookshelf from './modules/bookshelf.js';
 import hidepages from './modules/hidepages.js';
 
-const addBtn = document.getElementById('add');
-const listBtn = document.getElementById('list');
-const addNewBtn = document.getElementById('addNew');
-const contactBtn = document.getElementById('contact');
+window.addEventListener('load', () => {
+  const addBtn = document.getElementById('add');
+  const listBtn = document.getElementById('list');
+  const addNewBtn = document.getElementById('addNew');
+  const contactBtn = document.getElementById('contact');
 
-setTimeout(displayClock, 1000);
+  setTimeout(displayClock, 1000);
 
-const mybookshelf = new Bookshelf();
+  const mybookshelf = new Bookshelf();
 
-addBtn.addEventListener('click', mybookshelf.addNewBook);
-listBtn.addEventListener('click', hidepages);
-addNewBtn.addEventListener('click', hidepages);
-contactBtn.addEventListener('click', hidepages);
+  addBtn.addEventListener('click', mybookshelf.addNewBook);
+  listBtn.addEventListener('click', hidepages);
+  addNewBtn.addEventListener('click', hidepages);
+  contactBtn.addEventListener('click', hidepages);
 
-window.addEventListener('load', mybookshelf.retrievedata);
+  mybookshelf.retrievedata();
+});
